75896575abd581b58895de4913ec407eb72a5aeb
astralsandwich6324 <santillangarciaj659@gmail.com>
2026-04-26 15:45:34 -0400

    corrigiendo de forma manuel archivos del proyecto

--- POS.Api/Controllers/VentasController.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace POS.Api.Controllers
-{
-    public class VentasController
-    {
-    }
-}
--- POS.Api/Modelos/Request/InventoryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace POS.Api.Modelos.Request
-{
-    public class InventoryRequest : Inventory
-    {
-        public int Id { get; set; }
-    }
-}
